380be63b922c3d8a1ecae6e6a1f3e2cb3ed1383c
Mak Krnic <mkrnic@makDeskGentoo>
2012-12-07 11:56:45 +0100

    Dodana mogucnost odabira slika pri pozivu programa. Ucitavanje slike nije implementirano.

--- harrisMain.cpp
+++ harrisMain.cpp
@@ -11,6 +11,32 @@ int main (int argc, char *argv[]) {
   std::vector<CvPoint> points;
   CvPoint tmpPoint;
 
+  char usage[] = "-i ime_slike";
+  char opt;
+  char optstring[] = "i:";
+  char *imgName;
+
+  if (argc < 2) {
+    fprintf (stderr, "Upotreba: %s %s\n", argv[0], usage);
+    return 1;
+  }
+
+  while ((opt = getopt (argc, argv, optstring)) != -1) {
+    switch (opt) {
+      case 'i':
+        imgName = optarg;
+      break;
+
+      default:
+        fprintf (stderr, "Nepodrzana opcija.\n");
+        fprintf (stderr, "Upotreba: %s %s\n", argv[0], usage);
+        return 1;
+    }
+  }
+
+
+
+  // Testni podaci
   tmpPoint.x = 300;
   tmpPoint.y = 300;
 
