871d4b4f49c734d3b39ddca1a6fbe949490c6ac8
Mak Krnic <mkrnic@makDeskGentoo>
2012-12-07 17:04:54 +0100

    Uredjeno prikazivanje oznacenih tocaka.

--- harris.cpp
+++ harris.cpp
@@ -4,19 +4,29 @@
 #include "harris.hpp"
 
 void plotHarrisPoints (IplImage *image, std::vector<CvPoint>& filtered_coords) {
+  CvSize imSize = cvSize (image->width, image->height);
+
+  IplImage *imgColored = cvCreateImage (imSize, IPL_DEPTH_8U, 3);
+
+  cvCvtColor (image, imgColored, CV_GRAY2BGR);
+
   // crvena boja. Iz nekog razloga je BGR umjesto RGB.
   CvScalar pointColor = cvScalar (0, 0, 255);
 
   for (std::vector<CvPoint>::iterator point = filtered_coords.begin();
         point != filtered_coords.end(); ++point) {
     // Dodaj svaku tocku na sliku koja ce se prikazati.
-    cvCircle (image, *point, 5, pointColor, 5);
+    cvCircle (imgColored, *point, 5, pointColor, CV_FILLED);
   }
   
-  cvNamedWindow ("HarrisPoints", 0);
-  cvShowImage ("HarrisPoints", image);
+  cvNamedWindow ("HarrisPoints", CV_WINDOW_NORMAL | CV_WINDOW_KEEPRATIO | CV_GUI_NORMAL);
+  cvResizeWindow ("HarrisPoints", imSize.width, imSize.height);
+
+  cvShowImage ("HarrisPoints", imgColored);
   cvWaitKey ();
   cvDestroyWindow ("HarrisPoints");
+
+  cvReleaseImage (&imgColored);
   
   return;
 }
