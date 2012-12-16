47433f4c437b9bd0768e3e8ac815828828506926
Dino Kovač <dino.kovac@infinum.hr>
2012-12-16 17:51:17 +0100

    Popravio neke stvari da se kompajlira

--- harrisMain.cpp
+++ harrisMain.cpp
@@ -11,8 +11,8 @@
 
 int main (int argc, char *argv[]) {
   Mat img, imgBlurred;
-  std::vector<CvPoint> points;
-  CvPoint tmpPoint;
+  std::list<Point> points;
+  Point tmpPoint;
 
   char usage[] = "-i ime_slike";
   char opt;
