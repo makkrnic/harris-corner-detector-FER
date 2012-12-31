9c00576dd8d64cd7249fe1582b924f3f4f4b7382
Mak Krnic <makkrnic@gmail.com>
2012-12-31 01:54:12 +0100

    (Re)implemetiran gradijent. Prikazuje se otprilike ono sto bi trebalo.

--- computeHarrisResponse.cpp
+++ computeHarrisResponse.cpp
@@ -14,77 +14,85 @@ inline double getElement(Mat &matrix, int i, int j) {
     }
 }
 
-void computeHarrisResponse(Mat &src, Mat &dest) {
-      int scale = 1;
-      int delta = 0;
-      int ddepth = CV_16S;
-      int sigma = 3;
-      int ksize = 3; //ovo je velicina prozora za sobel funkciju...tipa 3x3
-      int wsize = 1;
+void calculate_xgrad (int w, int h, unsigned char *src, unsigned char * dst) {
+  int x,y, sum;
 
-      // TODO: provjeriti assertovima dimenzije i broj kanala
+  for (y = 1; y < h -1; y++) {
+    for (x = 1; x < w -1; x++) {
+      /***************************
+       * Racunamo x gradijent    *
+       * prema Sobelovoj matrici *
+       * -1 0 1                  *
+       * -2 0 2                  *
+       * -1 0 1                  *
+       ***************************/
+      sum =
+          -     src[(y - 1) * w + (x - 1)]
+          +     src[(y - 1) * w + (x + 1)]
+          - 2 * src[(y    ) * w + (x - 1)]
+          + 2 * src[(y    ) * w + (x + 1)]
+          -     src[(y + 1) * w + (x - 1)]
+          +     src[(y + 1) * w + (x + 1)];
 
-      Mat blurred = Mat(src.size(), CV_8UC1, Scalar::all(0));
-      grayscaleGaussianBlur(src, blurred, 11);
-
-      Mat derivX, derivY;
-      Scharr( blurred, derivX, ddepth, 1, 0, scale, delta, BORDER_DEFAULT ); //e sad prva gausova derivacija po x smjeru
-      Scharr( blurred, derivY, ddepth, 0, 1, scale, delta, BORDER_DEFAULT ); //prva gausova derivacija po y smjeru
-
-      Mat dx = Mat(derivX.size(), CV_8UC1);
-      Mat dy = Mat(derivY.size(), CV_8UC1);
-
-      // apsolutna vrijednost, tak sam vidio na primjeru u službenoj dokuemntaciji
-      convertScaleAbs(derivX, dx);
-      convertScaleAbs(derivY, dy);
-
-      // opet blur da se riješimo noise-a
-      grayscaleGaussianBlur(dx, dx, 5);
-      grayscaleGaussianBlur(dy, dy, 5);
-
-      // stvaram float matrice da izbjegnem overflow kod množenja
-      Mat Ixx = Mat(src.size(), CV_32F);
-      Mat Ixy = Mat(src.size(), CV_32F);
-      Mat Iyy = Mat(src.size(), CV_32F);
-
-      // ovaj dio mi je sumnjiv, iako je u teriji dobro
-      Ixx = dx.mul(dx);
-      Ixy = dx.mul(dy);
-      Iyy = dy.mul(dy);
-
-//      GaussianBlur( Ixx, Ixx, Size(0,0), sigma, BORDER_DEFAULT ); //opet mutim sa gausom za jos manji noise
-//      GaussianBlur( Ixy, Ixy, Size(0,0), sigma, BORDER_DEFAULT ); //u pythonu je lik mutil dva put pa sam i ja
-//      GaussianBlur( Iyy, Iyy, Size(0,0), sigma, BORDER_DEFAULT );
-
-//      convertScaleAbs(Ixx, Ixx);
-//      convertScaleAbs(Ixy, Ixy);
-//      convertScaleAbs(Iyy, Iyy);
-
-      Mat DetA = Mat(src.size(), CV_32F);
-      Mat DetB = Mat(src.size(), CV_32F);
-      Mat Trace = Mat(src.size(), CV_32F);
-      Mat Score = Mat(src.size(), CV_32F);
-      dest = Mat(src.size(), CV_32F);
-
-      DetA = Ixx.mul(Iyy); //za determinantu dijelovi
-      DetB = Ixy.mul(Ixy);
-      Mat Det = Mat(src.size(), CV_32F);;
-      subtract(DetA, DetB, Det); //tu izracunam determinantu
-
-      Trace = Ixx+Iyy;
-      divide(Det, Trace, Score);
-
-      // Make it all positive, because we're only interested in the size of the change, not direction
-      convertScaleAbs(Score, dest);
-
-      imshow("blur", blurred);
-      imshow("derivx", dx);
-      imshow("derivy", dy);
-      imshow("Ixx", Ixx);
-      imshow("Ixy", Ixy);
-      imshow("Iyy", Iyy);
-      imshow("dest", dest);
-
-      return ;
+      dst[y* w + x] = abs(sum);
+    }
+  }
 }
 
+void calculate_ygrad (int w, int h, unsigned char *src, unsigned char * dst) {
+  int x,y, sum;
+
+  for (y = 1; y < h -1; y++) {
+    for (x = 1; x < w -1; x++) {
+      /***************************
+       * Racunamo y gradijent    *
+       * prema Sobelovoj matrici *
+       * -1 -2 -1                *
+       *  0  0  0                *
+       *  1  2  1                *
+       ***************************/
+      sum =
+          -     src[(y - 1) * w + (x - 1)]
+          - 2 * src[(y - 1) * w + (x    )]
+          -     src[(y - 1) * w + (x + 1)]
+          +     src[(y + 1) * w + (x - 1)]
+          + 2 * src[(y + 1) * w + (x    )]
+          +     src[(y + 1) * w + (x + 1)];
+
+      dst[y* w + x] = abs(sum);
+    }
+  }
+}
+
+void computeHarrisResponse(Mat &src, Mat &dest) {
+  CvSize imgSize = src.size();
+
+  Mat srcBlurred = Mat (imgSize, CV_8UC1);
+  grayscaleGaussianBlur (src, srcBlurred, 5);
+  int numPixels = imgSize.width * imgSize.height;
+  unsigned char *imgData = (unsigned char *) malloc (sizeof (unsigned char) * numPixels);
+
+  memcpy (imgData, srcBlurred.data, sizeof (unsigned char) * numPixels);
+
+  unsigned char *xGrad = (unsigned char *) calloc (numPixels, sizeof (unsigned char));
+  unsigned char *yGrad = (unsigned char *) calloc (numPixels, sizeof (unsigned char));
+  
+
+  calculate_xgrad (imgSize.width, imgSize.height, imgData, xGrad);
+  calculate_ygrad (imgSize.width, imgSize.height, imgData, yGrad);
+
+  
+  
+
+  // Mat compatMat_XGrad = Mat (imgSize, CV_8UC1);
+  // compatMat_XGrad.data = xGrad;
+  // imshow ("Xgrad", compatMat_XGrad);
+  // 
+  // Mat compatMat_YGrad = Mat (imgSize, CV_8UC1);
+  // compatMat_YGrad.data = yGrad;
+  // imshow ("Ygrad", compatMat_YGrad);
+
+
+
+  return;
+}
